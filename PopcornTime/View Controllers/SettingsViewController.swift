9926851e46c1eae4d464160aef2396bb558908a6
User <none@yoogle.com>
2016-09-12 16:51:01 +0300

    fixed update dialog on settings

--- PopcornTime/View Controllers/SettingsViewController.swift
+++ PopcornTime/View Controllers/SettingsViewController.swift
@@ -323,18 +323,8 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             if indexPath.row == 2 {
                 UpdateManager.sharedManager().checkForUpdates(forVersion: version) { (updateAvailable, name, releaseNotes, error) in
                     if updateAvailable {
-                        let alertController = UIAlertController(title: "Update Available", message: nil, preferredStyle: .Alert)
-                        alertController.addTextFieldWithConfigurationHandler({ textField in
-                            //textField.text = "A new version of PopcornTime is available.\n\(name!)\n\n\(releaseNotes!)\n\nVisit https://github.com/PopcornTimeTV/PopcornTimeTV to update."
-                            let textView = UITextView()
-                            textView.text = "A new version of PopcornTime is available.\n\(name!)\n\n\(releaseNotes!)\n\nVisit https://github.com/PopcornTimeTV/PopcornTimeTV to update."
-                            let size = textView.sizeThatFits(CGSizeMake(textField.frame.width, CGFloat.max))
-                            textView.frame = CGRectMake(textField.bounds.origin.x, textField.bounds.origin.y, size.width, size.height)
-                            textField.removeFromSuperview()
-                            alertController.view.addSubview(textView)
-                        })
-                        alertController.addAction(UIAlertAction(title: "Close", style: .Cancel, handler: nil))
-                        self.presentViewController(alertController, animated: true, completion: nil)
+                        let alertRecipe = DescriptionRecipe(title: "Update Available", message: "A new version of PopcornTime is available.\n\(name!)\n\n\(releaseNotes!)\n\nVisit https://github.com/PopcornTimeTV/PopcornTimeTV to update.", buttons: [DescriptionButton.init(title: "Close", actionID: "closeAlert")], presentationType: .Modal) //we use this in order to prevent concateneted text on the alert! [FIX-IT] should be replaced with a swift way!
+                        Kitchen.serve(recipe: alertRecipe)
                     } else {
                         let alertController = UIAlertController(title: "No Updates Available", message: "You are using the latest version, \(self.version), however, if you are a developer, there might be a minor update avaible as a commit, you are using commit \(self.build), check https://github.com/PopcornTimeTV/PopcornTimeTV to see if new commits are available.", preferredStyle: .Alert)
                         alertController.addAction(UIAlertAction(title: "Close", style: .Cancel, handler: nil))
