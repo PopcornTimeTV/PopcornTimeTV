0fc334b1a8ca2ea0477555ae1445e7a50cdf63ee
User <aggelos@Aggelos-Macbook-Pro.local>
2018-05-03 15:31:02 +0300

    add a temp fix for tvos <11.3

--- PopcornTime/UI/tvOS/Extensions/SearchViewController+tvOS.swift
+++ PopcornTime/UI/tvOS/Extensions/SearchViewController+tvOS.swift
@@ -29,6 +29,17 @@ extension SearchViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        /*work-around to get the scopeBar to appear - NOTE: I need to find why it is happening and the
+         parent subview of the scopebar has an alpha of 0!!!!*/
+        if #available(tvOS 11.3, *){}else {
+            for sub in self.view.subviews{
+                for subv in sub.subviews{
+                    for subvi in subv.subviews{
+                        subvi.alpha = 1.0
+                    }
+                }
+            }
+        }
         super.viewDidAppear(animated)
         
         searchContainerViewController = searchContainerViewController ?? {
