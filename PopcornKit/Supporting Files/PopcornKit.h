ac78ea6250cd1add46d3141fe458ffa8e5dfcf00
User <aggelos@Aggelos-Macbook-Pro.local>
2018-07-01 21:50:49 +0300

    Remove attempts to fix airplay

--- PopcornKit/Supporting Files/PopcornKit.h
+++ PopcornKit/Supporting Files/PopcornKit.h
@@ -4,8 +4,7 @@
 #import <PopcornKit/MPAudioDeviceController.h>
 #import <PopcornKit/MPAVRoutingControllerDelegate.h>
 #import <PopcornKit/MPAVRoutingController.h>
-#import <PopcornKit/MPAVLightweightRoutingControllerDelegate.h>
-#import <PopcornKit/MPAVLightweightRoutingController.h>
+
 
 FOUNDATION_EXPORT double PopcornKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char PopcornKitVersionString[];
