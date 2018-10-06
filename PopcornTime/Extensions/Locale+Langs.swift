80760ab8559b990a503cd95409fef12be64d3315
User <aggelos@Aggelos-Macbook-Pro.local>
2018-10-06 12:47:58 +0300

    Update to v3.2.23

--- PopcornTime/Extensions/Locale+Langs.swift
+++ PopcornTime/Extensions/Locale+Langs.swift
@@ -6,7 +6,7 @@ extension Locale {
     
     /// Returns a list of common `Locale` ISO language codes.
     static var commonISOLanguageCodes: [String] {
-        return ["en", "fr", "de", "ja", "nl", "it", "es", "da", "fi", "no", "sv", "ko", "zh", "ru", "pl", "pt", "id", "tr", "hu", "el", "ca","bs","hr","sr", "hi", "th", "ms", "cs", "sk", "vi", "ro", "uk", "ar", "he"].sorted()
+        return ["en", "fr", "de", "ja", "nl", "it", "es", "da", "fi", "no", "sv", "ko", "zh", "ru", "pl", "pt", "id", "tr", "hu", "el", "ca","bs","hr","sr", "hi", "th", "ms", "cs", "sk", "vi", "ro", "uk", "ar", "he","sl"].sorted()
     }
     
     /// Returns a list of common `Locale` languages.
