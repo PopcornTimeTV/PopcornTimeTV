ee8174d6bd526180f88b9fbaab4b8563eee38976
User <aggelos@Aggelos-Macbook-Pro.local>
2018-12-18 22:55:02 +0200

    Fix subtitles not showing

--- PopcornTime/UI/tvOS/Extensions/PCTPlayerViewController+tvOS.swift
+++ PopcornTime/UI/tvOS/Extensions/PCTPlayerViewController+tvOS.swift
@@ -48,6 +48,7 @@ extension PCTPlayerViewController: UIViewControllerTransitioningDelegate {
         destinationController.modalPresentationStyle = .custom
         destinationController.delegate = self
         present(destinationController, animated: true)
+        destinationController.subtitlesViewController.subtitles = subtitles
         destinationController.subtitlesViewController.currentSubtitle = currentSubtitle
         destinationController.subtitlesViewController.currentDelay = mediaplayer.currentVideoSubTitleDelay/Int(1e6)
         destinationController.audioViewController.currentDelay = mediaplayer.currentAudioPlaybackDelay/Int(1e6)
