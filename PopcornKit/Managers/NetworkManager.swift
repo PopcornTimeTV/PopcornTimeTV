10f66f70c06d65639b042539f4c15bd914ba766b
r30 <46611237+r30@users.noreply.github.com>
2019-01-11 22:32:31 +0100

    Update NetworkManager.swift

--- PopcornKit/Managers/NetworkManager.swift
+++ PopcornKit/Managers/NetworkManager.swift
@@ -70,6 +70,9 @@ public struct Popcorn {
 
     static let movie = "/movie"
     static let show = "/show"
+    
+    static let userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.14"
+    static let defaultHeaders = ["User-Agent": Popcorn.userAgent]
 }
 
 public struct TMDB {
@@ -100,7 +103,7 @@ public struct Fanart {
 
 public struct OpenSubtitles {
     static let base = "https://rest.opensubtitles.org/"
-    static let userAgent = "Popcorn Time v1"
+    static let userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.14"
     
 //    static let logIn = "LogIn"
 //    static let logOut = "LogOut"
