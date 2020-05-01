79de0313066f4465c0322e50ddb1b7a91b4c44ed
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2020-04-30 23:54:49 -0400

    fixed undeclared variable

--- PopcornKit/Managers/SubtitlesManager.swift
+++ PopcornKit/Managers/SubtitlesManager.swift
@@ -75,6 +75,7 @@ open class SubtitlesManager: NetworkManager {
     }
     
     private func getParams(_ episode: Episode? = nil, imdbId: String? = nil,preferredLang: String? = nil,videoFilePath: URL? = nil, limit: String = "500") -> [String:Any] {
+        var params = [String:Any]()
         if let episode = episode {
             if let id = episode.show?.id {
                 params["imdbid"] = id.replacingOccurrences(of: "tt", with: "")
