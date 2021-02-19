9aeb7a4703038f6ee15060b12391b60573e9cbb7
Nick <nicksaintjohn@users.noreply.github.com>
2021-02-19 00:06:54 +0000

    Added UIVisualEffectView behind the media description for DescriptionViewController - Makes the detail text easier to read

--- PopcornTime/UI/tvOS/View Controllers/TVDescriptionViewController.swift
+++ PopcornTime/UI/tvOS/View Controllers/TVDescriptionViewController.swift
@@ -6,4 +6,12 @@ class TVDescriptionViewController: UIViewController {
     
     @IBOutlet var textView: UITextView!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet weak var blurBox: UIVisualEffectView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        blurBox?.layer.cornerRadius = 20
+        blurBox?.clipsToBounds = true
+    }
 }
